d234215d8f7f6b6533c90141f5e4053815ad998b
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-28 04:12:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,16 @@
 class Dog
-  def name=(name)
-
+  def name=(dogname)
+    doggiename = dogname
+  end
+  def name
+    doggiename
+  end
+  def bark
+    puts "woof"
+  end
 end
 
 Fido = Dog.new
+Fido.name = "Fido"
+Fido.name
+Fido.bark
